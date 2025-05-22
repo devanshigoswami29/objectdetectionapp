a593e46a47051f4939e7fae72a4c467901572443
devanshigoswami29 <167619192+devanshigoswami29@users.noreply.github.com>
2025-05-22 14:14:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+import torch
+
+st.title("Object Detection App (YOLOv5)")
+
+uploaded_file = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    model = torch.hub.load("ultralytics/yolov5", "yolov5s", trust_repo=True)
+    results = model(image)
+    results.render()
+    st.image(results.ims[0], caption="Detection Result", use_column_width=True)
